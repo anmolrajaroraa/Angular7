24c78eb2c60b99ac4711facbc2183026a95bb2ee
Anmol Arora <anmolarora1711@gmail.com>
2019-03-08 17:31:50 +0530

    Add files via upload

---
+++ firstapp/app/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-root', //by which name your component will be known/by which tag your component will be utilized
+  //template:'<h1>I am inline HTML coming from AppComponent</h1>',
+  templateUrl:'app.component.html',
+  styleUrls:["app.component.css"]
+})
+export class AppComponent {
+  
+  str:string;
+  myCssClass:string;
+  isHide:boolean;
+  counter:number;
+  result:number;
+  names:string[];
+  currentDate:object;
+  
+  constructor(){
+    this.str = "I am a string";
+    this.myCssClass = "yellow";
+    this.isHide = false;
+    this.counter = 0;
+    this.result = 0;
+    this.names = ["John", "Mike", "Emily", "Bob"];
+    this.currentDate = new Date();
+  }
+
+  toggleShowHide():void{
+    this.isHide = !this.isHide;
+  }
+
+  takeInput(event):void{
+    this.str = event.target.value;
+  }
+
+  incrementCounter():void{
+    (this.counter)++;
+  }
+
+  doAdd(x:string, y:string):void{
+    this.result =  parseInt(x) + parseInt(y);
+  }
+}
