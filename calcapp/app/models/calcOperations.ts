b98be159269c35689f69001b0a15812cec30723f
Anmol Arora <anmolarora1711@gmail.com>
2019-03-13 18:18:17 +0530

    Create calcOperations.ts

---
+++ calcapp/app/models/calcOperations.ts
@@ -0,0 +1,14 @@
+export const calcOperations={
+    add(x:number,y:number):number{
+        return x + y;
+    },
+    subtract(x:number,y:number):number{
+        return x - y;
+    },
+    multiply(x:number,y:number):number{
+        return x * y;
+    },
+    divide(x:number,y:number):number{
+        return x / y;
+    }
+}
