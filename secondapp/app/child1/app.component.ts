8006973dfdc86d09f17739abc52fa5a63b79e3e3
Anmol Arora <anmolarora1711@gmail.com>
2019-03-13 18:11:55 +0530

    Create app.component.ts

---
+++ secondapp/app/child1/app.component.ts
@@ -0,0 +1,27 @@
+import { AppComponent } from '../app.component';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child1',
+  templateUrl: './child1.component.html',
+  styleUrls: ['./child1.component.css']
+})
+export class Child1Component implements OnInit {
+
+  public message:string;
+  private confidentialInfo:string;
+  constructor() { 
+    this.message = "Default message";
+  }
+
+  ngOnInit() {
+  }
+
+  resetMessage(parentObject: AppComponent){
+    this.message = "Message has been reset";
+    console.log("Parent object is ",parentObject);
+    parentObject.callParent();
+  }
+
+
+}
